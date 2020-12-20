d85e4cd07c96de30afcc838a4f629e5652dac457
Sandesh <mankarsandesh11@gmail.com>
2020-12-20 22:43:59 +0800

    [FEAT] Home Page Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import logo from "../src/assets/logo.png";
 import Article from "../src/components/article//article";
 export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showInput: false,
+    };
+  }
+
   render() {
     const media =
       "https://www.bbc.co.uk/news/special/2015/newsspec_10857/bbc_news_logo.png?cb=1";
-    const mediaTitle = "The wrong criticism of his performance is to say that he has made mistakes";  
-    const mediaDesc = " Confronted with a novel disease for which the country was unprepared, any prime minister would have made errors. The correct criticism is that he has failed to learn from his mistakes and egregiously repeated them";
- 
+    const mediaTitle =
+      "The wrong criticism of his performance is to say that he has made mistakes";
+    const mediaDesc =
+      " Confronted with a novel disease for which the country was unprepared, any prime minister would have made errors. The correct criticism is that he has failed to learn from his mistakes and egregiously repeated them";
 
     return (
       <div className="App">
@@ -22,7 +30,17 @@ export default class Header extends Component {
                 <li className="yellow">Culture</li>
                 <li className="blue">Lifestyle</li>
               </ul>
-              <div className="header-search"></div>
+              <div className="header-search">
+                <i
+                  className="fa fa-search"
+                  onClick={() => {
+                    this.setState({ showInput: !this.state.showInput });
+                  }}
+                ></i>
+                {this.state.showInput ? (
+                  <input placeholder="Search all news" />
+                ) : null}
+              </div>
             </div>
           </div>
         </header>
@@ -33,27 +51,94 @@ export default class Header extends Component {
               <button className="bookmark">View Bookmark</button>
               <select className="search">
                 <option value="">Select Options</option>
-                <option value="">Latest News</option>
-                <option value="">Latest</option>
+                <option value="">Newest first</option>
+                <option value="">Oldest first</option>
+                <option value="">Most Popular</option>
               </select>
             </div>
           </div>
           <div className="container">
             <div className="container-left">
-              <Article media={media} mediaTitle={mediaTitle} mediaDesc={mediaDesc}/>
+              <Article
+                media={media}
+                mediaTitle={mediaTitle}
+                mediaDesc={mediaDesc}
+              />
             </div>
             <div className="container-right">
               <div className="col-6">
-              <Article media={media} mediaTitle={mediaTitle} mediaDesc={mediaDesc}/>
+                <Article
+                  media={media}
+                  mediaTitle={mediaTitle}
+                  mediaDesc={mediaDesc}
+                />
               </div>
               <div className="col-6">
-              <Article media={media} mediaTitle={mediaTitle} mediaDesc={mediaDesc}/>
+                <Article
+                  media={media}
+                  mediaTitle={mediaTitle}
+                  mediaDesc={mediaDesc}
+                />
               </div>
               <div className="col-6">
-              <Article  mediaTitle={mediaTitle} />
+                <Article mediaTitle={mediaTitle} />
               </div>
               <div className="col-6">
-              <Article mediaTitle={mediaTitle} />
+                <Article mediaTitle={mediaTitle} />
+              </div>
+            </div>
+          </div>
+          <div className="container-wrapper">
+            <div className="col-3">
+              <Article
+                media={media}
+                mediaTitle={mediaTitle}
+                mediaDesc={mediaDesc}
+              />
+            </div>
+            <div className="col-3">
+              <Article
+                media={media}
+                mediaTitle={mediaTitle}
+                mediaDesc={mediaDesc}
+              />
+            </div>
+            <div className="col-3">
+              <Article
+                media={media}
+                mediaTitle={mediaTitle}
+                mediaDesc={mediaDesc}
+              />
+            </div>
+          </div>
+          <div className="container-category">
+            <div className="category-header">
+              <h2>Sports</h2>
+              <div className="see-more">
+                <a href="#">See All</a>
+              </div>
+            </div>
+            <div className="container-wrapper">
+              <div className="col-3">
+                <Article
+                  media={media}
+                  mediaTitle={mediaTitle}
+                  mediaDesc={mediaDesc}
+                />
+              </div>
+              <div className="col-3">
+                <Article
+                  media={media}
+                  mediaTitle={mediaTitle}
+                  mediaDesc={mediaDesc}
+                />
+              </div>
+              <div className="col-3">
+                <Article
+                  media={media}
+                  mediaTitle={mediaTitle}
+                  mediaDesc={mediaDesc}
+                />
               </div>
             </div>
           </div>
